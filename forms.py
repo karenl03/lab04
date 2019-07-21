eb4cbf9840ebaf24850122d05e5e00e89eb1aada
Karen Li <karenli@karens-mac.local>
2019-07-20 23:06:21 -0400

    fixed add user

--- forms.py
+++ forms.py
@@ -3,11 +3,11 @@ from wtforms import StringField, FloatField, SubmitField, SelectField, IntegerFi
 from wtforms.validators import DataRequired
 
 class AddUserForm(FlaskForm):
-	username = StringField('Username', validators=[DataRequired()])
-	first_name = StringField('First Name', validators=[DataRequired()])
-	last_name = StringField('Last Name', validators=[DataRequired()])
-	prog_lang = SelectField('Programming Language', choices=[('cpp', 'C++'), ('py', 'Python'), ('java', 'Java'), ('php', 'PHP'), ('js', 'JavaScript'), ('other', 'Other')])
-	experience_yr = FloatField('Years of Programming Experience', validators=[DataRequired()])
-	age = IntegerField('Age', validators=[DataRequired()])
-	hw1_hrs = FloatField('Hours Spent on HW1', validators=[DataRequired()])
-	submit = SubmitField('Add')
+    username = StringField('Username', validators=[DataRequired()])
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    prog_lang = SelectField('Programming Language', choices=[('cpp', 'C++'), ('py', 'Python'), ('js', 'JavaScript'), ('java', 'Java'), ('php', 'PHP'), ('other', 'Other')])
+    experience_yr = FloatField('Years of Programming Experience', validators=[DataRequired()])
+    age = IntegerField('Age', validators=[DataRequired()])
+    hw1_hrs = FloatField('Hours Spent on HW 1', validators=[DataRequired()])
+    submit = SubmitField('Add')
--- routes.py
+++ routes.py
@@ -4,11 +4,10 @@ from forms import AddUserForm
 from flask_heroku import Heroku
 
 # Init Flask
-#app = Flask(__name__)
+app = Flask(__name__)
 
 #app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://localhost/homework_users'
 #app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-app = Flask(__name__)
 heroku = Heroku(app)
 db.init_app(app)
 app.secret_key = "dbd3"
@@ -22,22 +21,24 @@ def index():
 
 @app.route('/add-user', methods=['GET', 'POST'])
 def add_user():
-   form = AddUserForm()
-   if request.method == 'GET':
-       return render_template('add_user.html', form=form)
-   else:
-       if form.validate_on_submit():
-           username = request.form['username']
-           first_name = request.form['first_name']
-           last_name = request.form['last_name']
-           prog_lang = request.form['prog_lang']
-           experience_yr = request.form['experience_yr']
-           age = request.form['age']
-           hw1_hrs = request.form['hw1_hrs']
-           new_user = User(username=username, first_name=first_name, last_name=last_name, prog_lang=prog_lang, experience_yr=experience_yr, age=age, hw1_hrs=hw1_hrs)
-           db.session.add(new_user)
-           db.session.commit()
-           return redirect(url_for('index'))
+    form = AddUserForm()
+    if request.method == 'GET':
+        return render_template('add_user.html', form=form)
+    else:
+        #if form.validate_on_submit():
+        username = request.form['username']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        prog_lang = request.form['prog_lang']
+        experience_yr = request.form['experience_yr']
+        age = request.form['age']
+        hw1_hrs = request.form['hw1_hrs']
+        new_user = User(username=username, first_name=first_name, last_name=last_name, prog_lang=prog_lang, experience_yr=experience_yr, age=age, hw1_hrs=hw1_hrs)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('index'))
+        #else: return ("error")
+
 @app.route('/load_data', methods=['GET'])
 def load_data():
 	users_json = {'users': []}
